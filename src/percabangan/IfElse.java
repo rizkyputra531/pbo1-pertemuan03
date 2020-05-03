2362f713fe7f32d5b347e3a20bf677424db40a65
rizkyputra531 <rizkyputra531.rp@gmail.com>
2020-05-03 15:20:48 +0800

    Percabangan 2

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -2,12 +2,12 @@ package percabangan;
 
 public class IfElse {
     public static void main(String[] args) {
-        double totalBelanja = 53000;
-        double uangDiDompet = 250000;
+        double totalBelanja = 25000;
+        double uangDiDompet = 50000;
 
         if(uangDiDompet<totalBelanja){
             System.out.println("Uang kurang, kurangi jajan anda");
-        }else{
+        }else if(uangDiDompet>totalBelanja){
             System.out.println("Uang cukup, selamat menikmati");
         }
     }
