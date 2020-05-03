05413d798d1b708105a96f494ac0ac068d402c08
rizkyputra531 <rizkyputra531.rp@gmail.com>
2020-05-03 14:57:37 +0800

    Operator Unary

---
+++ src/operator/OperatorUnary.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class OperatorUnary {
+    public static void main(String[] args) {
+        int i = 0;
+        i++;                    //i tadinya 0 ditambahkan 1 sehingga i=1
+        System.out.println(i);  //betulkan i nilainya 1
+        i++;                    //karena i tadinya sudah bernilai 1 maka sekarang ditambahkan lagi 1 menjadi 2
+        System.out.println(i);  //betulkan i nilainya 2
+        i=i+1;                  //bisa dilihat i++ sama dengan i = i+1
+        System.out.println(i);  //i tadi 2 sekarang jadi 3
+    }
+}
