30d4ada3f10c09cae83e2b33431ed0ebac80e6af
rizkyputra531 <rizkyputra531.rp@gmail.com>
2020-05-03 14:28:33 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,11 @@
+package operator;
+
+public class OperatorBinary {
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar tidak dijalankan oleh program
+
+    String nama = "Rizky Syahputra";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+}
