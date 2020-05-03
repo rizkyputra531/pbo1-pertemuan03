c9231d3252c7b31ca989558a377b2cf3216cf727
rizkyputra531 <rizkyputra531.rp@gmail.com>
2020-05-03 14:37:21 +0800

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -8,4 +8,13 @@ public class OperatorBinary {
     String nama = "Rizky Syahputra";
     int jumlahSks = 24;
     double indeksPrestasiKumulatif = 3.89;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
 }
